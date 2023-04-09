abbbb111c76e91e9fb2566b34ef4840e17d8ee95
Lois K <amulya.lois@gmail.com>
2023-04-08 21:21:57 -0500

    Updates to remove redendancies

--- Resources/sql/customers_and_peak_hours.sql
+++ Resources/sql/customers_and_peak_hours.sql
@@ -1,11 +1,6 @@
 
 -- How many customers do we have each day? Are there any peak hours?
 
--- Exploring the orders table
-
--- Get all records from the table
-SELECT * FROM orders;
-
 -- Number of customers visiting the store each day
 SELECT 
 	order_date, 
@@ -14,14 +9,15 @@ FROM orders
 GROUP BY order_date
 ORDER BY order_date;
 
--- Peak hours per day
+-- Busiest times of day in the store
 SELECT 
-	order_date, 
 	EXTRACT(HOUR FROM order_time) AS order_hour, 
-	COUNT(*) AS  orders_received
-FROM orders 
-GROUP BY order_date, order_hour
-ORDER BY order_date ASC, orders_received DESC;
+	COUNT(*) AS orders_received
+FROM orders
+GROUP BY order_hour
+ORDER BY orders_received DESC
+LIMIT 3;
+
 
 -- Bonus Queries
 -- Describe the table structure
@@ -47,15 +43,6 @@ LIMIT 1;
 SELECT 
 	EXTRACT(HOUR FROM order_time) AS order_hour
 FROM orders;
-
--- Busiest times of day in the store (overall)
-SELECT 
-	EXTRACT(HOUR FROM order_time) AS order_hour, 
-	COUNT(*) AS orders_received
-FROM orders
-GROUP BY order_hour
-ORDER BY orders_received DESC
-LIMIT 3;
 
 
 
